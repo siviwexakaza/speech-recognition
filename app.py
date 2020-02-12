676bb5017e188abf9b9aefc22e509ebe3de39e55
siviwe <siviwe@fuzzelogicsolutions.com>
2020-02-12 12:48:19 +0200

    Respond to commands with text

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 import speech_recognition as speech
 
 recognizer = speech.Recognizer()
-with speech.Microphone() as mic:
-    print('Speak')
-    sound = recognizer.listen(mic)
-    mic_data = recognizer.recognize_google(sound)
-    print(mic_data)
-    
+
+def listen():
+
+    with speech.Microphone() as mic:
+        
+        sound = recognizer.listen(mic)
+        words =''
+        try:
+            words = recognizer.recognize_google(sound)
+            
+        except speech.UnknownValueError:
+            print('Can not understand that...')
+        except speech.RequestError:
+            print('Please check your internet connection...')
+        return words
+
+def command(cmd):
+    if 'what is your name' in cmd:
+        print('My name is Bella')
+
+
+print('Voice recognizer started, say something...')
+results = listen()
+command(results)
